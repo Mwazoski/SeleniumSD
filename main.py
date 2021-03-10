1e6229679939c1f6b906a2e3982d2aeb1b53b1d6
vivavomvi <mwazoski96@yahoo.com>
2021-03-10 10:58:27 +0100

    Base Selenium

---
+++ main.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("https://www.youtube.com/")
+        #self.assertIn("youtube", driver.title)
+        elem = driver.find_element_by_name("search_query")
+        elem.send_keys("ibai")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
